1ae154b5eb8ad13d69e75d2a270fc0288b7ca7f9
Gordon Olsen <gcolsenut@gmail.com>
2025-04-30 20:44:52 -0600

    started JS

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+let convertBtn = document.getElementById('convert-btn');
+let lengthVal = document.getElementById('length-val');
+let volumeVal = document.getElementById('volume-val');
+let weightVal = document.getElementById('weight-val');
+
+convertBtn.addEventListener('click', function () {
+  //lengthVal.textContent = 'Changed';
+  let convertNum = document.getElementById('convert-num').value;
+  console.log(convertNum);
+});
